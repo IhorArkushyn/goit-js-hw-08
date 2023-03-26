6fbf6b6e5004c96d87a3724fa48f0ad9e969c97f
Ihor Arkushyn <snapscan68@gmail.com>
2023-03-26 20:46:26 +0300

    add task_1 and all dependencies

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+// const SimpleLightbox = require('simplelightbox');
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('ul.gallery');
+galleryContainer.style.listStyle = 'none';
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ original, preview, description }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+    )
+    .join('');
+}
+
+const addGalleryMarcup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', addGalleryMarcup);
+
+galleryContainer.addEventListener('click', onGalleryImageClick);
+
+function onGalleryImageClick(event) {
+  const isGalleryEl = event.target.classList.contains('gallery__image');
+
+  event.preventDefault();
+
+  if (!isGalleryEl) {
+    return;
+  }
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
