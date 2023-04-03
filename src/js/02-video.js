67d3775ceb3e2bd9c0203e9b230cc27512b75290
Ihor Arkushyn <snapscan68@gmail.com>
2023-04-03 17:22:25 +0300

    add task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const getCurrentTime = function ({seconds}) {
+  localStorage.setItem(CURRENT_TIME_KEY, seconds) ;
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY) || 0);
