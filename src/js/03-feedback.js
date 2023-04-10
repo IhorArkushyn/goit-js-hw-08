13b4861a576bb84db2e159067c747ebd76e31e44
Ihor Arkushyn <snapscan68@gmail.com>
2023-04-10 16:23:04 +0300

    upd task_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,42 @@
+
+
 import throttle from 'lodash.throttle';
 
 const submitForm = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const formFieldEmail = submitForm.elements.email;
+const formFieldMessage = submitForm.elements.message;
 
 submitForm.addEventListener('input', throttle(onInputData, 500));
+submitForm.addEventListener('submit', onFormSubmit);
 
-const STORAGE_KEY = 'feedback-form-state';
 
-function onInputData(event) {
-  localStorage.setItem(
-    STORAGE_KEY,
-    JSON.stringify({
-      email: submitForm.elements.email.value,
-      message: submitForm.elements.message.value,
-    })
-  );
+let parseValues = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+function onInputData() {
+  parseValues = {
+    email: formFieldEmail.value,
+    message: formFieldMessage.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(parseValues));
 }
 
-submitForm.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
 
-  const savedData = localStorage.getItem(STORAGE_KEY);
-  const parseData = JSON.parse(savedData);
   console.log(
-    parseData ?? {
+    parseValues ?? {
       email: '',
-      message: ''
+      message: '',
     }
   );
-
+ 
   submitForm.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
+
+ if (parseValues) {
+   formFieldEmail.value = parseValues.email ?? '';
+   formFieldMessage.value = parseValues.message ?? '';
+ }
